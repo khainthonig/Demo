64fca86233bff063fe31f1af180745a81fcf49f4
Admin <Admin@MSI>
2024-06-04 15:51:00 +0700

    Add project files.

---
+++ Dataman/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Cognex.DataMan.SDK;
+using Cognex.DataMan.SDK.Discovery;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Dataman
+{
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<EthSystemDiscoverer.SystemInfo> _ethDevices;
+        private EthSystemDiscoverer _ethSystemDiscoverer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeEthSystemDiscoverer();
+        }
+
+        private void InitializeEthSystemDiscoverer()
+        {
+            _ethDevices = new ObservableCollection<EthSystemDiscoverer.SystemInfo>();
+            ethDevicesListBox.ItemsSource = _ethDevices;
+
+            _ethSystemDiscoverer = new EthSystemDiscoverer();
+            _ethSystemDiscoverer.SystemDiscovered += OnEthSystemDiscovered;
+        }
+
+        private async void OnEthSystemDiscovered(EthSystemDiscoverer.SystemInfo systemInfo)
+        {
+            await Dispatcher.InvokeAsync(() => _ethDevices.Add(systemInfo));
+        }
+
+        private async void DiscoverEthDevicesButton_Click(object sender, RoutedEventArgs e)
+        {
+            _ethDevices.Clear();
+            await DiscoverEthDevicesAsync();
+        }
+
+        private async Task DiscoverEthDevicesAsync()
+        {
+            await Task.Run(() => _ethSystemDiscoverer.Discover())
+                .ContinueWith(task =>
+                {
+                    if (_ethDevices.Count == 0)
+                    {
+                        MessageBox.Show("Không có thiết bị Ethernet được phát hiện.");
+                    }
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+    }
+}
---
+++ Dataman/ScanData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dataman
+{
+    public class ScanData : INotifyPropertyChanged
+    {
+        private string _scannedBarcode;
+        public string ScannedBarcode
+        {
+            get { return _scannedBarcode; }
+            set
+            {
+                _scannedBarcode = value;
+                OnPropertyChanged("ScannedBarcode");
+            }
+        }
+
+        private string _scanDate;
+        public string ScanDate
+        {
+            get { return _scanDate; }
+            set
+            {
+                _scanDate = value;
+                OnPropertyChanged("ScanDate");
+            }
+        }
+
+        private string _barcodeType;
+        public string BarcodeType
+        {
+            get { return _barcodeType; }
+            set
+            {
+                _barcodeType = value;
+                OnPropertyChanged("BarcodeType");
+            }
+        }
+
+        private string _scanTime;
+        public string ScanTime
+        {
+            get { return _scanTime; }
+            set
+            {
+                _scanTime = value;
+                OnPropertyChanged("ScanTime");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
